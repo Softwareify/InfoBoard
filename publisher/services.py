8e6079cbcbe3b92d94f96e67f9ef7f408be2d10d
Michał Wrona <wronamichal01@gmail.com>
2023-10-07 22:36:26 +0200

    feat(project): Refactor project and added publisher

--- publisher/services.py
+++ publisher/services.py
@@ -9,9 +9,10 @@ class PublicationService:
 
     @classmethod
     def add_task(cls, *, type_publication: str, object_id: int, due_date: datetime):
-        return cls.model.objects.create(
+        defaults = {"type_publication": type_publication, "object_id": object_id, "due_date": due_date, "execution_date": None}
+        return cls.model.objects.filter(
             type_publication=type_publication, object_id=object_id, due_date=due_date
-        )
+        ).update_or_create(defaults=defaults)
 
     @staticmethod
     def page_publish(page_id: int):
