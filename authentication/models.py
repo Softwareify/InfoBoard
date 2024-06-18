16c0b5448c886ccd61aab399a6e9be22be3bb895
Michał Wrona <wronamichal01@gmail.com>
2024-06-18 22:40:36 +0200

    Update

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+@receiver(post_save, sender=get_user_model())
+def post_save(*_args, **_kwargs):
+    instance = _kwargs.get("instance", {})
+    if instance._state.db == "default":
+        instance.save(using="public")
